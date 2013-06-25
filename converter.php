534bdff31add042d76d7f27ae701f41ae82e2de6
ajayver <ajayver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-25 08:37:23 +0000

    Initial upload
    git-svn-id: https://plugins.svn.wordpress.org/converter/trunk@731407 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ converter.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Converter
+Plugin URI: http://ajayver.com
+Description: This plugin adds a widget that allows users to convert different values
+Version: 1.0
+Author: ajayver
+Author URI: http://ajayver.com
+License: GPL2
+*/
+
+
+include( plugin_dir_path( __FILE__ ) . 'widget.php');
+
